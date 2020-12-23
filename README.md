a732f49765261a17f7a9d3647a28872969265fa3
J4YP33 <jonathanpung@gmail.com>
2020-12-24 00:13:17 +0800

    created sqlite db

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CRUD_Flask
+
+https://www.youtube.com/watch?v=Z1RJmh_OqeA
+
+- run create_db.py to create db
--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)  # references this file
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"  # db name here
+db = SQLAlchemy(app)  # init db with app settings
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"<task {self.id}>"
 
 
 @app.route("/")
